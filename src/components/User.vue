0da8ca363d9da1f970d70fcc09b46850feede7aa
Louis <louis.vanoise@coqliqo.com>
2022-10-27 16:24:15 +0200

    add feature edit user

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,20 +6,55 @@ export default {
       email: "email@email.fr",
       timeSpendToday: 6,
       timeSpendMonth: 25,
+      editUsername: false,
+      editEmail: false,
+      handleBtnSubmit: false,
     };
   },
-  methods: {},
+  methods: {
+    handleEdit() {
+      this.editEmail = !this.editEmail;
+      this.editUsername = !this.editUsername;
+      this.handleBtnSubmit = !this.handleBtnSubmit;
+    },
+    submitEdit() {
+      this.username = document.getElementById("username").value;
+      this.email = document.getElementById("email").value;
+      this.handleBtnSubmit = !this.handleBtnSubmit;
+      this.editEmail = !this.editEmail;
+      this.editUsername = !this.editUsername;
+    },
+  },
 };
 </script>
 <template>
   <div
-    class="w-64 bg-lightGray flex flex-col items-center rounded-lg py-5 space-y-3"
+    class="w-64 h-72 bg-lightGray flex flex-col items-center rounded-lg py-5 space-y-3"
   >
     <i class="fa-solid fa-user text-4xl"></i>
-    <p>{{ username }}</p>
-    <p>{{ email }}</p>
+    <p v-if="!editUsername">{{ username }}</p>
+    <input
+      v-if="editUsername"
+      type="text"
+      :value="username"
+      id="username"
+      class="input"
+    />
+    <p v-if="!editEmail">{{ email }}</p>
+    <input
+      v-if="editEmail"
+      type="text"
+      :value="email"
+      id="email"
+      class="input"
+    />
     <p>Temps passé aujourd'hui: {{ timeSpendToday }}h</p>
     <p>Temps passé ce mois-ci: {{ timeSpendMonth }}h</p>
-    <button class="btn">Modifier</button>
+    <button v-if="!handleBtnSubmit" @click="handleEdit" class="btn">
+      Modifier
+    </button>
+    <button v-if="handleBtnSubmit" @click="submitEdit" class="btn">
+      Valider
+    </button>
   </div>
 </template>
